095d0e4d1a38694b2a826aefbd91f5f45b7005e8
Aiman Abdullah Anees <aimanabdullah65@gmail.com>
2016-09-28 22:49:43 +0530

    Updated Readme

--- ViewController.swift
+++ ViewController.swift
@@ -7,8 +7,13 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController , UITableViewDelegate,UITableViewDataSource {
+class ViewController: UIViewController , UITableViewDelegate,UITableViewDataSource ,NSFetchedResultsControllerDelegate {
+    //NSFetchedResultsControllerDelegate plays a very important role in fetching certain data into the table at a particular time rather than fetching the entire data..
+    
+    var fetchedResultsController: NSFetchedResultsController<Item>!
+    
     
     @IBOutlet var tableView: UITableView!
     
@@ -19,6 +24,9 @@ class ViewController: UIViewController , UITableViewDelegate,UITableViewDataSour
         // Do any additional setup after loading the view, typically from a nib.
         tableView.delegate = self //Setting the delegate
         tableView.dataSource = self //Setting the datasource
+        generateTestData() //if we comment this without saveContext()..it will not appear as the data is not yet passed on to the database
+        
+        attemptFetch()
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,16 +36,134 @@ class ViewController: UIViewController , UITableViewDelegate,UITableViewDataSour
 
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemCell
+        configureCell(cell: cell, indexPath: indexPath as NSIndexPath)
+        
+        return cell
+    }
+    
+    func configureCell(cell:ItemCell, indexPath: NSIndexPath){
+        let item = fetchedResultsController.object(at: (indexPath as! NSIndexPath) as IndexPath)
+        cell.configureCell(item: item)
+        
+        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if let sections = fetchedResultsController.sections{
+            let sectionInfo = sections[section]
+            return sectionInfo.numberOfObjects
+        }
+        
         return 0
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
+        if let sections = fetchedResultsController.sections{
+            return sections.count
+        }
         return 0
     }
+    
+    //Below is done to maintain the cell height
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+    
+    
+    //Once we have data in the database,we need a function to fetch the data and then display it..following is used for this purpose.
+    func attemptFetch(){
+        let fetchRequest: NSFetchRequest<Item> = Item.fetchRequest()
+        //"Item"--data model created earlier
+        let dateSort = NSSortDescriptor(key: "created", ascending: false)
+        fetchRequest.sortDescriptors = [dateSort]
+        
+        //Activates the above code..
+        let controller = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context , sectionNameKeyPath: nil, cacheName: nil)
+        //"context---refer AppDelegate.swift
+        
+        self.fetchedResultsController = controller
+        
+        do{
+            try fetchedResultsController.performFetch()
+        }catch{
+            let error = error as! NSError
+            print("\(error)")
+            
+        }
+        
+    }
+    
+    //Add,Delete,and Update functions
+    
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+        
+    }
+    
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        
+        switch(type){
+        case.insert:
+            if let indexPath = newIndexPath{
+                tableView.insertRows(at: [indexPath], with: .fade)
+                
+            }
+            break
+            
+        case.delete:
+            if let indexPath = indexPath{
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+            break
+        
+        case.update:
+            if let indexPath = indexPath{
+                let cell = tableView.cellForRow(at: indexPath) as! ItemCell
+                configureCell(cell: cell, indexPath: indexPath as NSIndexPath)
+                
+            }
+            break
+            
+        case.move:
+            if let indexPath = indexPath{
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+            if let indexPath = indexPath{
+                tableView.insertRows(at: [indexPath], with: .fade)
+                
+            }
+            break
+        
+        }
+        
+    }
+    
+    func generateTestData(){
+        let item = Item(context: context)
+        item.title = "MacBook Pro"
+        item.price = 1800
+        item.details = "I can't wait until September event"
+        
+        let item2 = Item(context: context)
+        item2.title = "Bose Headphone"
+        item2.price = 300
+        item2.details = "Awesome headphones"
+        
+        let item3 = Item(context: context)
+        item3.title = "Tesla Model S"
+        item3.price = 110000
+        item3.details = "A beautiful Car.."
+        
+        ad.saveContext()
+        
+        }
 
 }
 
